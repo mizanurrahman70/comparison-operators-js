4eee88147acc3dff25182784429b214daed95ff3
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-01-26 22:38:06 -0800

    multiple logic oparetor

---
+++ ml-logic.js
@@ -0,0 +1,21 @@
+// Multiple Logic condition chack 
+
+const slary = 24000;
+const BCS= true  ;
+const hight = 7;
+if (slary <23000 && BCS == true){
+    console.log(' ai hai eida hoilo suu patro')
+}
+else{
+    console.log('nah tmk diya kaj hoibo na bhai . amra arekta patro khuji')
+}
+// more and more condtion 
+if (slary < 2100 || 
+    hight < 7 || BCS == true  ){
+    console.log(
+        'ami raji '
+    )
+}
+else{
+    console.log('onnota khuji')
+}
