4887f336a31fa104460521815b8fcdb01c96c883
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-01-26 23:56:06 -0800

    added my code

--- multi-if-else.js
+++ multi-if-else.js
@@ -22,3 +22,30 @@ else if (Price >= 2000){
 else {
     console.log(Price)
 }
+
+
+//  children free condtion 
+
+const age = 30 ;
+const price =3000;
+if (age <=17)
+{
+    console.log('you are totaly free ')
+}
+else if ( age >= 61){
+    // 50% discount
+    const discount = price * 50 / 100 ;
+    var payAmount= price - discount
+    console.log(payAmount)
+    
+}
+else if (age >= 40){
+    // 40% 
+    var discount = price * 40 / 100 ;
+
+    var payAmount = price - discount
+    console.log(payAmount)
+}
+else{
+    console.log(price)
+}
