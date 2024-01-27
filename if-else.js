cfe44c6f468454613c64d53b0d2daa18c959c281
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-01-26 22:07:24 -0800

    first commit

---
+++ if-else.js
@@ -0,0 +1,21 @@
+// This is if else introdction 
+let biraniPrice= 500;
+// structer for conditon 
+
+// if (condtion ){
+//     out put this code 
+// }
+
+if (biraniPrice < 500){
+    console.log('mama ajke biranikhabo ami')
+}
+else{
+    console.log('taile ajke onno kichu khano buhso mama')
+}
+const wight = 7 ;
+if (wight < 6){
+    console.log('taile mama amr darai hobe na ami eto ojon niya jaite parmu na ')
+}
+else{
+    console.log(' taile ami mathai koira niya jaite partmu beper na eidi bhai')
+}
